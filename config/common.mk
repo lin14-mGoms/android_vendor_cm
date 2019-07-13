4061f66a5a62b945e58268286b01cab79c076692
MSe <mse1969@posteo.de>
2019-07-13 21:34:54 +0200

    Implement OTA updates
     - Add back the LineageOS Updater.apk
     - Generate a property for a static updater json resource
    
    Change-Id: Iaec701f2938beb42d2c1b98c7cbc104ba844b4e1

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,10 @@ PRODUCT_OTA_PUBLIC_KEYS := $(OWN_KEYS_DIR)/releasekey
 PRODUCT_EXTRA_RECOVERY_KEYS := $(OWN_KEYS_DIR)/releasekey
 endif
 
+# Updater URI
+PRODUCT_PROPERTY_OVERRIDES += \
+    cm.updater.uri=https://raw.githubusercontent.com/lin14-mGoms/OTA/cm-14.1-oms/$(CM_BUILD).json
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -133,6 +137,7 @@ PRODUCT_PACKAGES += \
     LockClock \
     ThemeInterfacer \
     Trebuchet \
+    Updater \
     WallpaperPicker \
     WeatherProvider
 
