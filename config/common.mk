84a3f875b3512a1da6020449ee7866ff50d2ac35
MSe <mse1969@posteo.de>
2022-06-16 16:24:30 +0200

    Custom build version 2202-06-01
    Change-Id: If12853e4c4433cfccf1e55d91eb6a53f3bb8b928

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2022-05-01
+ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2022-06-01
 
 # microG packages
 PRODUCT_PACKAGES += \
