4533ea0c5a0b820970fe21a44550eb954d62ec71
MSe <mse1969@posteo.de>
2023-04-21 00:00:40 +0200

    Custom build version 2023-04-01
    Change-Id: Iaf46158b108ec8b751d858f7ee3538872d514629

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 PRODUCT_BRAND ?= LineageOS
 
 # Unofficial patch level
-ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2023-03-01
+ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2023-04-01
 
 # Sign with own key
 ifneq ($(OWN_KEYS_DIR),)
