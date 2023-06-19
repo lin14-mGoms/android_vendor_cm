16f3e0e053df860fb8f9e16c039780f251f1bed3
MSe <mse1969@posteo.de>
2023-06-19 23:09:23 +0200

    Custom build version 2023-06-01
    Change-Id: Ie839242d2a963edf090d2dc1ca44146e99f7407f

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2023-05-01
+ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2023-06-01
 
 # microG packages
 PRODUCT_PACKAGES += \
