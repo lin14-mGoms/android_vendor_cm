ab7d9d632e8d8dc23d44fbd67b6e19c8ff41567e
mse1969 <mse1969@posteo.de>
2024-09-23 19:55:06 +0200

    Custom build release 2024-09-01
    Change-Id: I3f140af077df200726fa391eb5e36cec1b43d87a

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2024-08-01
+ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2024-09-01
 
 # microG packages
 PRODUCT_PACKAGES += \
