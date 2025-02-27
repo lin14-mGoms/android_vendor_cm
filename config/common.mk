4f9570bf87f13629c7d1d10ddb862e12f677bbde
mse1969 <mse1969@posteo.de>
2025-02-27 21:56:41 +0100

    Custom build release 2025-02-01
    Change-Id: I162e7c4095f70667b60495edb346c636d26a6a01

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 PRODUCT_BRAND ?= LineageOS
 
 # Unofficial patch level
-ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2025-01-01
+ADDITIONAL_DEFAULT_PROPERTIES += ro.cm.custom_version=2025-02-01
 
 # Cromite webview
 PRODUCT_PACKAGES += cromite-webview
